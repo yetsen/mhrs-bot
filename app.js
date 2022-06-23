39dcb700b1ff9096160d97cf46d49a45d8daf3ce
Yunus Emre Tekin <yunusemretekin1@gmail.com>
2022-06-23 07:50:55 +0300

    logs are added

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ async function main() {
     let citySelector = 'span[title="' + CREDS.city +'"]';
     let clinicSelector = 'span[title="' + CREDS.clinic +'"]';
 
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({headless: true});
+    console.log("starting... " + new Date());
     const context = browser.defaultBrowserContext();
     await context.overridePermissions("https://mhrs.gov.tr", ['geolocation']);
     const page = await browser.newPage();
@@ -19,8 +20,6 @@ async function main() {
     await page.goto('https://mhrs.gov.tr/vatandas/#/', { waitUntil: 'networkidle0' }); // wait until page load
 
     await page.evaluate((mhrsUser, mhrsToken, isAuth) => {
-            console.log(mhrsUser);
-            console.log(mhrsToken);
             localStorage.setItem("users-v-mhrs", mhrsUser);
             localStorage.setItem("token-v-mhrs", mhrsToken);
             localStorage.setItem("isAuth-v-mhrs", isAuth);
@@ -29,10 +28,13 @@ async function main() {
     await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
     let loginElement = (await page.$('#LoginForm_username')) || "";
     if (loginElement !== "") {
+        console.log("Login is required " + new Date());
         await page.type('#LoginForm_username', CREDS.username);
         await page.type('#LoginForm_password', CREDS.password);
 
         await page.click('button[type=submit]');   
+    } else {
+        console.log("Login is not required " + new Date())
     }
 
     await page.waitForSelector('.hasta-randevu-card')
@@ -50,8 +52,7 @@ async function main() {
     await page.waitForSelector('label[title=City]');
     await page.waitForTimeout(500);
 
-    let endDate = new Date();
-    endDate.setDate(endDate.getDate()+5);
+    console.log("Search for appointment page is opened " + new Date());
     await page.click('input[placeholder="Starting Date Please Select"]');
     await page.waitForSelector('.ant-calendar-today-btn');
     await page.click('.ant-calendar-today-btn');
@@ -59,17 +60,22 @@ async function main() {
     await page.keyboard.type(CREDS.lastDate);
     await page.keyboard.press('Enter');
 
+    console.log("Dates are filled " + new Date());
+
     await page.waitForSelector('#vatandasApp > section > main > div > div.randevu__container > div > div.ant-card-body > div > div.ant-tabs-content.ant-tabs-content-animated.ant-tabs-top-content > div > div.rand-arama__container > form > div:nth-child(3) > div.ant-col.ant-form-item-control-wrapper > div > span > span.ant-select-enabled');
     await page.click('#vatandasApp > section > main > div > div.randevu__container > div > div.ant-card-body > div > div.ant-tabs-content.ant-tabs-content-animated.ant-tabs-top-content > div > div.rand-arama__container > form > div:nth-child(3) > div.ant-col.ant-form-item-control-wrapper > div > span > span');
     await page.waitForTimeout(500);
     await page.click(citySelector);
 
+    console.log("City is filled " + new Date());
 
     await page.waitForSelector('#vatandasApp > section > main > div > div.randevu__container > div > div.ant-card-body > div > div.ant-tabs-content.ant-tabs-content-animated.ant-tabs-top-content > div > div.rand-arama__container > form > div:nth-child(5) > div.ant-col.ant-form-item-control-wrapper > div > span > span.ant-select-enabled')
     await page.waitForTimeout(500);
     await page.click('#vatandasApp > section > main > div > div.randevu__container > div > div.ant-card-body > div > div.ant-tabs-content.ant-tabs-content-animated.ant-tabs-top-content > div > div.rand-arama__container > form > div:nth-child(5) > div.ant-col.ant-form-item-control-wrapper > div > span > span > span')
     await page.waitForTimeout(500);
     await page.click(clinicSelector);
+
+    console.log("Clinic is filled " + new Date());
 
     if (CREDS.hospital !== '') {
         await page.waitForTimeout(500);
@@ -81,6 +87,7 @@ async function main() {
         if (button) {
             await button.click();
         }
+        console.log("Hospital is filled " + new Date());
     }
 
     await page.waitForSelector('button[type=submit]:not([disabled])');
@@ -89,7 +96,7 @@ async function main() {
     try {
         await page.waitForSelector('.ant-list-item:nth-child(1)');
     } catch {
-        console.log("appointment not taken");
+        console.log("appointment not taken " + new Date());
         page.close();
         browser.close();
         return
@@ -113,7 +120,7 @@ async function main() {
     await page.waitForSelector('button.slot-saat-button[type=button]:not([disabled])');
     await page.waitForTimeout(1000);
     await page.click('button.slot-saat-button[type=button]:not([disabled])');
-
+    console.log("Hour is selected " + new Date());
     try {
         await page.waitForSelector('.ant-modal-confirm-btns > button[type=button]', {timeout: 3000})
         await page.waitForTimeout(500);
